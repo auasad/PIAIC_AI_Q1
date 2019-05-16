3e1cfe0c2d3c4f9f28432450f34b7ebce2a7ff0d
Asad Ullah <asad.yzai@gmail.com>
2019-05-16 05:46:06 +0500

    variables in Python

---
+++ Python_Practices/variables-in-python.py
@@ -0,0 +1,17 @@
+### integers and fload
+num1 = 123
+num2 = 100.98
+sum = num1 + num2
+print(sum)
+
+### strings
+first_name = "Asad"
+last_name = "Ullah"
+print(first_name + " " + last_name)
+
+
+### characters
+char1 = 'a'
+char2 = 'A'
+print(char2 + " is a CAPITAL while " + char1 + " is a small.")
+
